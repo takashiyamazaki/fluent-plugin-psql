2f34b95880ddc6a9a49df6af9626f1e4ee9070d7
takashiyamazaki <mt.zakitaka@gmail.com>
2013-01-10 20:17:00 +0900

    store hstore type.

--- lib/fluent/plugin/out_psql.rb
+++ lib/fluent/plugin/out_psql.rb
@@ -1,26 +1,32 @@
-class Fluent::PsqlOutput < Fluent::Output
+class Fluent::PsqlOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('psql', self)
 
-  # config_param :hoge, :string, :default => 'hoge'
+  config_param :database, :string
+  config_param :host, :string, :default => 'localhost'
+  config_param :port, :integer, :default => 5432
+  config_param :user, :string
+  config_param :password, :string
+
+  config_param :key_names, :string
+  config_param :sql, :string
+
+  config_param :hstore_key_name, :string, :default => nil
 
   def initialize
     super
-    # require 'hogehoge'
+    require 'pg'
   end
 
   def configure(conf)
     super
-    # @path = conf['path']
   end
 
   def start
     super
-    # init
   end
 
   def shutdown
     super
-    # destroy
   end
 
   def format(tag, time, record)
@@ -28,10 +34,47 @@ class Fluent::PsqlOutput < Fluent::Output
   end
 
   def write(chunk)
-    records = []
-    chunk.msgpack_each { |record|
-      # records << record
+    conn = get_connection
+    p @sql
+    conn.prepare("insert",@sql)
+
+    chunk.msgpack_each { |tag,time,record|
+      p record
+
+      if !@hstore_key_name.nil? and record.has_key?(@hstore_key_name)
+        hstore_ary = []
+        elems = record[@hstore_key_name].split("&")
+        elems.each do |elem|
+          kv = elem.split("=")
+          hstore_ary.push("\"#{kv[0]}\" => \"#{kv[1]}\"")
+        end
+      end
+      puts ["'#{record["method"]}'","'#{record["code"]}'", "'#{hstore_ary.join(",")}'"]
+
+      begin
+        conn.exec_prepared('insert',["#{record["method"]}","#{record["code"]}", "#{hstore_ary.join(",")}"])
+      rescue PGError => e
+        $log.error "PGError: " + e.message
+      end
     }
-    # write records
+
+    conn.close()
   end
+
+  def get_connection
+    if @conn != nil and @conn.finished?() == false
+      return @conn
+    end
+
+    begin
+      @conn = PG.connect(:dbname => @database, :host => @host, :port => @port,
+                         :user => @user, :password => @password)
+    rescue PGError => e
+      $log.error "database '#{@database}' connection failed : " + e.message
+      return nil
+    end
+
+    return @conn
+  end
+
 end
--- test/plugin/test_out_psql.rb
+++ test/plugin/test_out_psql.rb
@@ -6,10 +6,16 @@ class PsqlOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %{
+    database hoge
+    user hoge_user
+    password hoge_password
+
+    key_names fuga_key
+    sql fuga_sql
   }
 
   def create_driver(conf = CONFIG, tag = 'test')
-    Fluent::Test::OutputTestDriver.new(Fluent::PsqlOutput, tag).configure(conf)
+    Fluent::Test::BufferedOutputTestDriver.new(Fluent::PsqlOutput, tag).configure(conf)
   end
 
   def test_configure
@@ -20,6 +26,22 @@ class PsqlOutputTest < Test::Unit::TestCase
   end
 
   def test_write
-    d = create_driver
+    d = create_driver %[
+      database log_db
+      user tester
+      password tester
+
+      key_names method,code
+      sql INSERT INTO simple_apache_access (method,code,data) VALUES ($1,$2,$3)
+
+      hstore_key_name params
+
+      flush_interval 5s
+    ]
+    time = Time.parse('2012-12-17 09:23:45 JST').to_i # JST(+0900)
+    record = {"method"=>"GET", "code"=>"200", "params" => "param1=hoge&param2=fuga"}
+    d.emit(record, time)
+    d.run
   end
+
 end
